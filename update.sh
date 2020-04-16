0a05c191464825031a1528f6131e2cb4c672dedc
Riley Mathews <rileymathews80@gmail.com>
2020-04-15 22:33:56 -0500

    removed use of aws2

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-aws2 cloudformation update-stack --stack-name main-vpc --template-body file://`pwd`/cloudformation.yml
+aws cloudformation update-stack --stack-name main-vpc --template-body file://`pwd`/cloudformation.yml
